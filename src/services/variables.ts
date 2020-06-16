337f894c282eb22435de5a12cba6531e591b31c7
RomSribn <steeper.fest@gmail.com>
2020-06-16 10:50:35 +0300

    creating config.js that includes necessary env vaiables

--- src/services/variables.ts
+++ src/services/variables.ts
@@ -1,8 +1,11 @@
+import { weatherConfig } from 'config';
 import { IApiOpenweatherRequest } from './interfaces';
 
-export const apiIpifyRequest: string = `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_IPIFY_API_KEY}`;
+const { openWeatherApiKey, ipifyApiKey } = weatherConfig;
+
+export const apiIpifyRequest: string = `https://geo.ipify.org/api/v1?apiKey=${ipifyApiKey}`;
 export const apiOpenweatherRequest = ({
   lat,
   lng,
 }: IApiOpenweatherRequest): string =>
-  `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=hourly&appid=${process.env.REACT_APP_APP_OPENWEATHER_API_KEY}`;
+  `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=hourly&appid=${openWeatherApiKey}`;
